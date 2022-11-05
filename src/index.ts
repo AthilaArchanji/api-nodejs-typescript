54408f6cb945acdf2023ede4057d5f94df7b9bc7
Athila <athila.rodriguess@gmail.com>
2022-11-05 15:39:03 -0300

    Ultimas mudanças

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
-
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) =>{
+    res.json({
+        msg: 'ok'
+    })
+});
+
+app.listen(PORT, () => console.log(`Servidor iniciado na porta ` + PORT));
---
+++ src/modules/product.ts
@@ -0,0 +1,5 @@
+export type Produtc = {
+    id: number;
+    name: string;
+    price: number;
+}
